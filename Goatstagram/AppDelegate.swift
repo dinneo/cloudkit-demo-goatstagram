235785c10dd4db5488507071f253a90970fdba66
Tomasz Szulc <mail@szulctomasz.com>
2015-10-24 17:24:13 -0700

    Create project

---
+++ Goatstagram/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Goatstagram
+//
+//  Created by Tomasz Szulc on 24/10/15.
+//  Copyright © 2015 Tomasz Szulc. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
